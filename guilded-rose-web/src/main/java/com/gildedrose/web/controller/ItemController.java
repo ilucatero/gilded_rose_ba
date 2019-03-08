90ff8b3576046d36b2f65380f1ee756f1e9d6e54
ilucatero <wizard_indarkness@hotmail.com>
2019-03-08 14:42:50 +0100

    add base item controller class

--- guilded-rose-web/src/main/java/com/gildedrose/web/controller/ItemController.java
+++ guilded-rose-web/src/main/java/com/gildedrose/web/controller/ItemController.java
@@ -1,4 +1,17 @@
 package com.gildedrose.web.controller;
 
+import com.guildedrose.core.model.Item;
+import com.guildedrose.core.service.ItemService;
+
+import java.util.List;
+
 public class ItemController {
+
+    // TODO: replace by the spring bean
+    ItemService itemService = new ItemService();
+
+    public List<Item> getItems(){
+        return itemService.getItems();
+    }
+    
 }
