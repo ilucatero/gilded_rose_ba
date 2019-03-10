d3dc79a636d76c5efc624ef9a67ed1c3584d00e5
ilucatero <wizard_indarkness@hotmail.com>
2019-03-10 19:41:15 +0100

    add degrade button

--- guilded-rose-web/src/main/resources/static/js/script.js
+++ guilded-rose-web/src/main/resources/static/js/script.js
@@ -10,13 +10,30 @@ function loadItems(){
             var table = $('#inventory').DataTable({
                 data: dataSet,
                 columns: [
+                    { data: "id", visible: false },
                     { data: "name", title: "Name" },
                     { data: "sellIn", title: "Sell In"  },
                     { data: "quality", title: "Quality"  },
-
+                    { data: "tag", title: "Tags", "defaultContent": ""  },
+                    { // Actions column
+                        "className":      'item-actions',
+                        "orderable":      false,
+                        "data":           null,
+                        "title":          "Actions",
+                        "defaultContent": '<a class="button ok item-degrade">Degrade</a>'
+                    },
                 ]
             });
 
+            $('#inventory tbody')
+                .on('click', 'td.item-actions .item-degrade', function () {
+                    var tr = $(this).closest('tr');
+                    var row = table.row( tr );
+                    console.debug(row.data());
+                    var itemId = row.data().id;
+                  // TODO: call ms to degrade the selected item id
+                    // TODO: if success update dataTable data values for quality/sellIn
+                } );
         } catch (e) {
             console.error(e);
         }
