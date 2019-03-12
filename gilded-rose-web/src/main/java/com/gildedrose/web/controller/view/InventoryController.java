bd1e7328f98e774e9a021fdb75b00b4a3e3c5e26
ilucatero <wizard_indarkness@hotmail.com>
2019-03-12 21:01:23 +0100

    add base InventoryController & TODO

---
+++ gilded-rose-web/src/main/java/com/gildedrose/web/controller/view/InventoryController.java
@@ -0,0 +1,12 @@
+package com.gildedrose.web.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    // TODO: use this view instead of root entry-point once the site layouts and menus are implemented
+
+}
