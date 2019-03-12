ab483a77e107a47f625b3878b67399c35b78d0da
ilucatero <wizard_indarkness@hotmail.com>
2019-03-12 19:49:00 +0100

    add some todos on root entrypoint

--- gilded-rose-web/src/main/java/com/gildedrose/web/controller/RootController.java
+++ gilded-rose-web/src/main/java/com/gildedrose/web/controller/RootController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RootController {
 
+    // Note: at the moment there is no other page to go, but in case any the root path should forward to main page
+    // TODO: implement security, authentication (OAUTH jtw/sessions) & redirection to login page
+
     @GetMapping("/")
     public String index(Model model) {
+        // TODO: to implement internationalization, get messages and texts from properties files (e.g. messages-fr.properties)
+
         model.addAttribute("message", "Inventory of Items");
         return "index";
     }
