04d8c172747865f123263c443f659fd98f9cc033
ilucatero <wizard_indarkness@hotmail.com>
2019-03-31 00:28:39 +0100

    add logout functionality

--- gilded-rose-web/src/main/java/com/gildedrose/web/controller/RootController.java
+++ gilded-rose-web/src/main/java/com/gildedrose/web/controller/RootController.java
@@ -1,12 +1,17 @@
 package com.gildedrose.web.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class RootController {
@@ -33,4 +38,13 @@ public class RootController {
         return new RedirectView("/");
     }
 
+    @GetMapping(value="/logout")
+    public RedirectView logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        //You can redirect wherever you want, but generally it's a good practice to show login screen again.
+        return  new RedirectView("/login?logout=true");
+    }
 }
